1289d7a279a73324e038f0765223f56864a5b88a
Damylet <mirenadamylet@gmail.com>
2024-12-17 18:03:49 -0300

    se agrago carrusel css

--- public/js/funcion.js
+++ public/js/funcion.js
@@ -4,7 +4,7 @@ function typeWriter(element, text, delay, callback) {
     function typing() {
         if (i < text.length) {
             if (text.charAt(i) === '.') {
-                element.innerHTML += '.<br>'; // Añadir salto de línea después del punto
+                element.innerHTML += '.<br>'; 
                 i++;
             } else {
                 element.innerHTML += text.charAt(i);
@@ -23,8 +23,8 @@ window.onload = function() {
     const descripcion = document.getElementById('descripcion');
     
     titulo.classList.remove('hidden');
-    typeWriter(titulo, 'Hola soy, Damylet Mirena', 100, () => {
+    typeWriter(titulo, 'Hola soy, Damylet Mirena', 60, () => {
         descripcion.classList.remove('hidden');
-        typeWriter(descripcion, 'Programador Full Stack con conocimientos en desarrollo web y aplicaciones. Poseo habilidades en HTML / CSS / JavaScript / Nodejs / Java / Spring Boot. Lo que me permite crear sitios web escalables y robustos. Me apasiona aumentar mis conocimientos y habilidades.', 50);
+        typeWriter(descripcion, 'Tengo conocimientos en desarrollo web y aplicaciones que me han permitido crear algunos proyectos que puedes ver en la sección de proyectos. Poseo habilidades en HTML / CSS / JavaScript / Nodejs / Java / Spring Boot. Lo que me permite crear sitios web escalables y robustos. Me apasiona aumentar mis conocimientos y habilidades.', 30);
     });
 }
